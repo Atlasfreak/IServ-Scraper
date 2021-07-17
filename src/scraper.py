d3ee58077a75213d0782c9f43fab75e29d78e2b4
Per <atlasfreak@gmx.de>
2021-07-17 15:56:11 +0200

    restructured file; added filters;

--- src/scraper.py
+++ src/scraper.py
@@ -1,8 +1,21 @@
 # %%
-from bs4.element import Tag
 import requests
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
+
+# %%
+def href_filter(href: str):
+    return href.startswith(url + "/iserv/exercise/show/")
+
+
+def tag_filter(tag: Tag):
+    return (
+        tag.name == "a" and href_filter(tag["href"]) and "AG 3D-Druck" not in tag.string
+    )
+
+
+#%%
 client = requests.Session()
 username = input("Username: ")
 password = input("Password: ")
@@ -15,21 +28,15 @@ page_login = client.post(login_url, data=login_info)
 if page_login.url.startswith(login_url):
     print("Something went wrong.")
     raise Exception
+else:
+    print("Logged in")
 
 # %%
 page_exercise = client.get(url + "/iserv/exercise?filter[status]=all")
 
-# %%
-def href_filter(href: str):
-    return href.startswith(url + "/iserv/exercise/show/")
-
-
-def tag_filter(tag: Tag):
-    return tag.name == "a" and href_filter(tag["href"])
-
 
 # %%
 soup_page_exercise = BeautifulSoup(page_exercise.text, "html.parser")
-soup_page_exercise.find_all("a", href=href_filter)
+filtered_exercises = soup_page_exercise.find_all(tag_filter)
 
 # %%
